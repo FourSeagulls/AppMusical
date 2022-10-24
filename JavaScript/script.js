11ba6a08aa98991714712200b6b3f7dadc4de73b
FourSeagulls <114670998+FourSeagulls@users.noreply.github.com>
2022-10-24 23:19:03 +0200

    Introducción del teclado del piano y sus sonidos

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,3 +2,18 @@ function funciona() {
     console.log("La página se cargó correctamente");
     console.log("La Música estará sonando en breve");
 }
+
+/* + + + + + + + SONODOS DEL TECLADO + + + + + + + + + + + + + + + + + + + + + + */
+var context= new AudioContext();
+ 
+function jsNota(frecuencia){
+        var o= context.createOscillator();
+        g=context.createGain();
+        o.connect(g);
+        o.type="sawtooth";
+        o.frequency.value=frecuencia;
+        g.connect(context.destination);
+        o.start(0);
+        g.gain.exponentialRampToValueAtTime(0.00001,context.currentTime +1.5);
+    }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - -  FIN - SONIDOS DEL TECLADO */    
